f5202aa4cdc0d21f2eeffa6e61a3c16fe9923408
Agustin Tassano <agustin.tassano@gmail.com>
2022-12-16 15:37:03 +0100

    feat; pasar de pc de tto a la mia

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,54 @@
+// src/App.vue
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-
-  <i class="fas fa-trophy"></i>
+  <h1>IronContacts</h1>
+  <button @click="addRandomContact()">Add Random Contact</button>
+<table>
+  <tr>
+    <th>Picture</th>
+    <th>Name</th>
+    <th>Popularity</th>
+    <th>Won Oscar</th>
+    <th>Won Emmy</th>
+  </tr>
+  <tr v-for="contact in firstFiveContacts">
+    <td><img :src="contact.pictureUrl" :alt="`${contact.name} picture`" width="100"></td>
+    <td>{{ contact.name }}</td>
+    <td>{{ contact.popularity }}</td>
+    <td v-if="contacts.wonOscar">
+      <i class="fas fa-trophy"></i>
+    </td>
+    <td v-if="contacts.wonEmmy">
+      <i class="fas fa-trophy"></i>
+    </td>
+  </tr>
+</table>
 </template>
 
 <script>
-export default {
-
-}
+import CONTACTS from "./contacts.json"
+  export default {
+    data() {
+      return {
+        contacts: CONTACTS,
+        firstFiveContacts: CONTACTS.slice(0, 5),
+      }
+    },
+    computed: {
+    
+    },
+    methods: {
+      addRandomContact() {
+        const randomContact = this.contacts[Math.floor(Math.random()*this.contacts.length)]
+        console.log(randomContact)
+        return this.firstFiveContacts.push(randomContact)
+      }
+    }
+    // mounted() {
+    //   this.addRandomContact
+    // }
+  } 
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
+
